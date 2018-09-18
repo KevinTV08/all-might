a6701730e27f2fbb5dc351f7a1611f98d66425ab
Kevin <kevinntrann@gmail.com>
2018-09-18 12:45:38 -0700

    readme.md update

---
+++ main.js
@@ -0,0 +1,11 @@
+const $toggleButton = $('.toggle-button');
+$toggleButton.on('click', e => {
+  $('.mobile-nav').toggle('show');
+});
+
+const $shopBtn = $('.shop-btn');
+$shopBtn.on('click', e => {
+  $('.submenu').toggle('show');
+});
+
+
